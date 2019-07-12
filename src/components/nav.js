b048cb4cca7f3405eb281f96e2f43fc5379e07b9
Darlene Zouras <dzouras@gmail.com>
2019-07-12 16:21:04 -0500

    updated for nav styling between mobile and desktop

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,6 +5,9 @@ import styles from "./nav.module.css"
 
 export default () => (
     <nav id="nav" className={styles.nav}>
+        <div className={styles.nav_pull_mobile}>
+            ^
+        </div>
         <ul>
             <li>
                 <Link to="/">Home</Link>
