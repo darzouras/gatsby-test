e55c9493e6b7cf71dc99ccd2a8098b9180c10436
Darlene Zouras <dzouras@gmail.com>
2019-07-31 11:30:18 -0500

    updated location for blog posts

--- src/components/blogpost.js
+++ src/components/blogpost.js
@@ -1,8 +1,14 @@
 import React from "react"
 import styles from "./blogpost.module.css"
 
-export default () => (
+export default ({ data }) => (
     <div className={styles.blogWrapper} >
         <h1>title?</h1>
     </div>
 )
+
+export const query = graphql`
+    query {
+        
+    }
+`
