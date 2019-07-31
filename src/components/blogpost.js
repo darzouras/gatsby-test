7d65a43a127968a4acbb21e7ae2c14ba0d4c1eac
Darlene Zouras <dzouras@gmail.com>
2019-07-31 11:21:31 -0500

    added 404, starting to add blog post and blogroll page

---
+++ src/components/blogpost.js
@@ -0,0 +1,8 @@
+import React from "react"
+import styles from "./blogpost.module.css"
+
+export default () => (
+    <div className={styles.blogWrapper} >
+        <h1>title?</h1>
+    </div>
+)
---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "../components/header"
+import Layout from "../components/layout"
+
+export default () => (
+    <Layout>
+
+        <Header headerText="Missing" />
+
+        <h2>Error - 404</h2>
+        <p>Oops, this page is missing!</p>
+
+    </Layout>
+)
