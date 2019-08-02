1f7de75b8b2567eb72349ef116a9e29ff5ccd60d
Darlene Zouras <dzouras@gmail.com>
2019-08-02 10:29:47 -0500

    corrected frameBorder error

--- src/components/tunes.js
+++ src/components/tunes.js
@@ -3,6 +3,6 @@ import styles from "./tunes.module.css"
 
 export default () => (
     <div className={styles.tunes}>
-        <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1E4xrPeIxCx9BP" frameborder="0" allowtransparency="true" allow="encrypted-media" title="spotify-playlist"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/37i9dQZF1E4xrPeIxCx9BP" frameBorder="0" allowtransparency="true" allow="encrypted-media" title="spotify-playlist"></iframe>
     </div>
 )
