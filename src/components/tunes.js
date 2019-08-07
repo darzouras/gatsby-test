4cadcb8b8c9eeb684c9589dffc77184ddf5f4b15
Darlene Zouras <dzouras@gmail.com>
2019-08-07 14:58:44 -0500

    updating for new cms fields

--- src/components/tunes.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import styles from "./tunes.module.css"
-
-export default (props) => (
-    <div className={styles.tunes}>
-        <iframe src={props.iframeSrc} frameBorder="0" allowtransparency="true" allow="encrypted-media" title="spotify-playlist"></iframe>
-    </div>
-)
--- src/pages/test.md
+++
@@ -1,6 +0,0 @@
----
-title: "This is a test!!"
-date: "2019-07-16"
----
-
-test test test
