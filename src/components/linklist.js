e64c28a00488b5fe8fb362b3cfb36b85cb23490f
Darlene Zouras <dzouras@gmail.com>
2019-08-07 14:43:15 -0500

    added link bar

--- src/components/linklist.js
+++ src/components/linklist.js
@@ -1,31 +1,35 @@
 import React from "react"
 import styled from "@emotion/styled"
+import { FaHandPeace, FaGithub, FaLinkedin, FaTwitter, FaInstagram } from 'react-icons/fa'
 
 const LinkWrapper = styled.aside`
     position: relative;
-    width: 90%;
-    margin: 25px auto;
+    margin: 25px;
     color:  #e0e0ce;
     background: #3d2c49;
     border-radius: 10px;
-    border: 3px solid #3d2c49;
-    min-height: 110px;
-    font-size: 1.2em;
+    font-size: 1.7em;
 
     @media (min-width: 768px) {
-        grid-column: 1/3;
-        grid-row: 3/6;
+        position: fixed;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        width: 100%;
+        border-radius: 0px;
+        margin: 0;
     }
 `
 
 const LinkList = styled.ul`
-    margin: 25px auto;
-    width: 90%;
+    padding: 25px;
     list-style: none;
+    display: flex;
+    flex-wrap: nowrap;
 `
 
 const LinkListItem = styled.li`
-    margin-bottom: 5px;
+    margin-right: 25px;
 
     a {
         color: #e0e0ce;
@@ -37,19 +41,19 @@ export default () => (
     <LinkWrapper>
         <LinkList>
             <LinkListItem>
-                <a href="https://darzouras.com" target="_blank">DarZouras.com</a>
+                <a href="https://darzouras.com" target="_blank" rel="noopener noreferrer">< FaHandPeace /></a>
             </LinkListItem>
             <LinkListItem>
-                <a href="https://github.com/darzouras" target="_blank">Github</a>
+                <a href="https://github.com/darzouras" target="_blank" rel="noopener noreferrer">< FaGithub /></a>
             </LinkListItem>
             <LinkListItem>
-                <a href="http://linkedin.com/in/darlenezouras" target="_blank">LinkedIn</a>
+                <a href="http://linkedin.com/in/darlenezouras" target="_blank" rel="noopener noreferrer">< FaLinkedin /></a>
             </LinkListItem>
             <LinkListItem>
-                <a href="https://twitter.com/darzouras" target="_blank">Twitter</a>
+                <a href="https://twitter.com/darzouras" target="_blank" rel="noopener noreferrer">< FaTwitter /></a>
             </LinkListItem>
             <LinkListItem>
-                <a href="https://www.instagram.com/darzouras/" target="_blank">Instagram</a>
+                <a href="https://www.instagram.com/darzouras/" target="_blank" rel="noopener noreferrer">< FaInstagram /></a>
             </LinkListItem>
         </LinkList>
     </LinkWrapper>
